a57763f72b65a24f9e30510fcd3730d24b2f01b1
yeongbba <yeongmolee2@gmail.com>
2020-10-21 22:21:50 +0900

    make token with jsonwebtoken

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const config = require("./config/key");
+const config = require('./config/key');
 
-const { User } = require("./models/User");
+const { User } = require('./models/User');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 //mongoDB, mongoose 연결
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -19,19 +21,45 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!!!");
+app.get('/', (req, res) => {
+  res.send('Hello World!!!!!');
 });
 
-app.post("/register", (req, res) => {
+app.post('/register', (req, res) => {
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '제공된 이메일에 해당하는 유저가 없습니다.',
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: '비밀번호가 틀렸습니다.',
+        });
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        //토끈 저장
+        res
+          .cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
   });
 });
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -33,10 +34,10 @@ const userSchema = mongoose.Schema({
   },
 });
 
-userSchema.pre("save", function (next) {
+userSchema.pre('save', function (next) {
   let user = this;
 
-  if (user.isModified("password")) {
+  if (user.isModified('password')) {
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
 
@@ -46,9 +47,30 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  } else {
+    next();
   }
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (callback) {
+  let user = this;
+
+  let token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
