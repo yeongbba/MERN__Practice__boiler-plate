f8dbe303f05f82a069876d356dc84af636066ca0
yeongbba <yeongmolee2@gmail.com>
2020-10-24 16:09:00 +0900

    set up React router&proxy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -27,6 +26,10 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!!!');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send('안녕');
 });
 
 app.post('/api/users/register', (req, res) => {
@@ -85,6 +88,8 @@ app.get('/api/users/logout', auth, (req, res) => {
     });
   });
 });
+
+const port = 5000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
