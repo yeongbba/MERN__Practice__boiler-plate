a356d28147114863417da13d4ed58dd649877e25
yeongbba <yeongmolee2@gmail.com>
2020-10-27 00:44:22 +0900

    Redux set up & make login page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,19 @@ function LandingPage() {
   useEffect(() => {
     axios.get('/api/hello').then((response) => console.log(response));
   }, []);
-  return <div>LandingPage</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <h2>시작 페이지</h2>
+    </div>
+  );
 }
 
 export default LandingPage;
