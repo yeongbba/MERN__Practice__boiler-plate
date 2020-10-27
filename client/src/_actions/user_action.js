fc3f83f9fcbcf891ab88595d2ed2a9c1df53c53f
yeongbba <yeongmolee2@gmail.com>
2020-10-27 21:41:47 +0900

    make register form

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataToSubmit) {
   const request = axios
@@ -11,3 +11,14 @@ export function loginUser(dataToSubmit) {
     payload: request,
   };
 }
+
+// export function registerUser(dataToSubmit) {
+//   const request = axios
+//     .post('/api/users/register', dataToSubmit)
+//     .then((response) => response.data);
+
+//   return {
+//     type: REGISTER_USER,
+//     payload: request,
+//   };
+// }
